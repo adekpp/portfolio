5bb3915364b0eeb7aa56879847ea920ab2143e93
adekpp <adekpp@gmail.com>
2022-05-04 21:57:34 +0200

    added new project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,10 @@
 import React from "react";
 import "./Projects.css";
-import weather from "../img/weather.jpg";
-import gallery from "../img/gallery.jpg";
-import recipes from "../img/recipes.jpg"
-import fishing from "../img/fishing.jpg"
+import weatherImg from "../img/weather.jpg";
+import galleryImg from "../img/gallery.jpg";
+import recipesImg from "../img/recipes.jpg";
+import fishingImg from "../img/fishing.jpg";
+import shoppingCartImg from "../img/shoppingCart.jpg";
 import { useDoubleTap } from "use-double-tap";
 import { BrowserView, MobileView } from "react-device-detect";
 import { useContext } from "react";
@@ -20,162 +21,222 @@ const Wrapper = styled.div`
 export const Projects = () => {
   const { mode } = useContext(ThemeContext);
 
-  const app1 = useDoubleTap((event) => {
+  const gallery = useDoubleTap((event) => {
     event.preventDefault();
     event.stopPropagation();
     window.open("https://gallery-api-unsplash-demo.herokuapp.com/");
-     return false;
-    
   });
-  const app2 = useDoubleTap((event) => {
+  const weather = useDoubleTap((event) => {
     event.preventDefault();
     event.stopPropagation();
-   
     window.open("https://app-weather-now.herokuapp.com/");
   });
 
-  const app3 = useDoubleTap((event) => {
+  const recipes = useDoubleTap((event) => {
     event.preventDefault();
     event.stopPropagation();
-  
     window.open("https://cooking-recipes-db254.web.app/");
   });
 
-  const app4 = useDoubleTap((event) => {
+  const fishingSpots = useDoubleTap((event) => {
     event.preventDefault();
     event.stopPropagation();
-  
+
     window.open("https://gdzienaryby.netlify.app/");
+  });
+
+  const shoppingCart = useDoubleTap((event) => {
+    event.preventDefault();
+    event.stopPropagation();
+
+    window.open("https://shopping-cart-adrian-pietryga.netlify.app/");
   });
 
   return (
     <div className="projects flex flex-grow flex-col  mx-auto text-gray-900 select-none ">
-    <h1 className={`text-center md:text-2xl ${mode}`}>Projects:</h1>
-      <div className="flex flex-col mt-8 md:mt-32 md:flex-row flex-grow flex-nowrap justify-start md:justify-center place-items-start gap-6">
+      <h1 className={`text-center md:text-2xl ${mode}`}>Projects:</h1>
+      <div className="flex flex-col mt-8 md:mt-32 md:flex-row flex-grow flex-wrap justify-start md:justify-center place-items-start gap-6">
+        <Wrapper>
+          <MobileView>
+            <div
+              {...shoppingCart}
+              className="img-wrapper flex flex-row  place-items-center justify-center text-white drop-shadow-md select-none"
+            >
+              <div className="absolute text-xl bg-pink-500 bg-opacity-60 rounded-md p-2">
+                <p className="drop-shadow-sm">Shopping Cart</p>
+              </div>
 
-      <Wrapper>
-      <MobileView>
-      <div {...app4} className="img-wrapper flex flex-row  place-items-center justify-center text-white drop-shadow-md select-none">
-        <div className="absolute text-xl bg-pink-500 bg-opacity-60 rounded-md p-2">
-          <p className="drop-shadow-sm">Gdzie na ryby?</p>
-        </div>
+              <img src={shoppingCartImg} alt="weather" />
+              <div className="content text-gray-900 select-none">
+                Redux toolkit - shopping cart
+              </div>
+            </div>
+          </MobileView>
+          <BrowserView>
+            <a
+              href="https://shopping-cart-adrian-pietryga.netlify.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <div className="img-wrapper flex flex-row  place-items-center justify-center text-white drop-shadow-md select-none">
+                <div className="absolute text-xl bg-pink-500 bg-opacity-60 rounded-md p-2">
+                  <p className="drop-shadow-sm">Shopping Cart</p>
+                </div>
 
-        <img src={fishing} alt="weather" />
-        <div className="content text-gray-900 select-none">
-        Apllication for anglers to share good fishing spots.         
-        </div>
-      </div>
-      </MobileView>
-      <BrowserView>
-      <a 
-      href="https://gdzienaryby.netlify.app/" target="_blank"
-      rel="noopener noreferrer">
-      <div className="img-wrapper flex flex-row  place-items-center justify-center text-white drop-shadow-md select-none">
-        <div className="absolute text-xl bg-pink-500 bg-opacity-60 rounded-md p-2">
-          <p className="drop-shadow-sm">Gdzie na ryby?</p>
-        </div>
-        
-        <img src={fishing} alt="weather" />
-        <div className="content text-gray-900 select-none">
-        Apllication for anglers to share good fishing spots.
-        </div>
-      </div></a>
-      </BrowserView>
-    </Wrapper>
+                <img src={shoppingCartImg} alt="weather" />
+                <div className="content text-gray-900 select-none">
+                  Redux toolkit - shopping cart
+                </div>
+              </div>
+            </a>
+          </BrowserView>
+        </Wrapper>
 
         <Wrapper>
           <MobileView>
-          <div {...app2} className="img-wrapper flex flex-row  place-items-center justify-center text-white drop-shadow-md select-none">
-            <div className="absolute text-xl bg-pink-500 bg-opacity-60 rounded-md p-2">
-              <p className="drop-shadow-sm">Weather App</p>
-            </div>
+            <div
+              {...fishingSpots}
+              className="img-wrapper flex flex-row  place-items-center justify-center text-white drop-shadow-md select-none"
+            >
+              <div className="absolute text-xl bg-pink-500 bg-opacity-60 rounded-md p-2">
+                <p className="drop-shadow-sm">Gdzie na ryby?</p>
+              </div>
 
-            <img src={weather} alt="weather" />
-            <div className="content text-gray-900 select-none">
-            A simple app to check the current weather.<br/>
-             App is using OpenWeatherMap API. /Double tap to open.
+              <img src={fishingImg} alt="weather" />
+              <div className="content text-gray-900 select-none">
+                Apllication for anglers to share good fishing spots.
+              </div>
             </div>
-          </div>
           </MobileView>
           <BrowserView>
-          <a 
-          href="https://app-weather-now.herokuapp.com/" target="_blank"
-          rel="noopener noreferrer">
-          <div className="img-wrapper flex flex-row  place-items-center justify-center text-white drop-shadow-md select-none">
-            <div className="absolute text-xl bg-pink-500 bg-opacity-60 rounded-md p-2">
-              <p className="drop-shadow-sm">Weather App</p>
+            <a
+              href="https://gdzienaryby.netlify.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <div className="img-wrapper flex flex-row  place-items-center justify-center text-white drop-shadow-md select-none">
+                <div className="absolute text-xl bg-pink-500 bg-opacity-60 rounded-md p-2">
+                  <p className="drop-shadow-sm">Gdzie na ryby?</p>
+                </div>
+
+                <img src={fishingImg} alt="weather" />
+                <div className="content text-gray-900 select-none">
+                  Apllication for anglers to share good fishing spots.
+                </div>
+              </div>
+            </a>
+          </BrowserView>
+        </Wrapper>
+
+        <Wrapper>
+          <MobileView>
+            <div
+              {...weather}
+              className="img-wrapper flex flex-row  place-items-center justify-center text-white drop-shadow-md select-none"
+            >
+              <div className="absolute text-xl bg-pink-500 bg-opacity-60 rounded-md p-2">
+                <p className="drop-shadow-sm">Weather App</p>
+              </div>
+
+              <img src={weatherImg} alt="weather" />
+              <div className="content text-gray-900 select-none">
+                A simple app to check the current weather.
+                <br />
+                App is using OpenWeatherMap API.
+              </div>
             </div>
-            
-            <img src={weather} alt="weather" />
-            <div className="content text-gray-900 select-none">
-            A simple app to check the current weather.
-             App is using OpenWeatherMap API.
-            </div>
-          </div></a>
+          </MobileView>
+          <BrowserView>
+            <a
+              href="https://app-weather-now.herokuapp.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <div className="img-wrapper flex flex-row  place-items-center justify-center text-white drop-shadow-md select-none">
+                <div className="absolute text-xl bg-pink-500 bg-opacity-60 rounded-md p-2">
+                  <p className="drop-shadow-sm">Weather App</p>
+                </div>
+
+                <img src={weatherImg} alt="weather" />
+                <div className="content text-gray-900 select-none">
+                  A simple app to check the current weather. App is using
+                  OpenWeatherMap API.
+                </div>
+              </div>
+            </a>
           </BrowserView>
         </Wrapper>
         <Wrapper>
-        <MobileView>
-          <div {...app1}
-           className="img-wrapper flex flex-row         place-items-center justify-center text-white drop-shadow-md select-none">
-            <div 
-            className="absolute text-xl  bg-pink-500 bg-opacity-75 rounded-md p-2">
-              <p className="drop-shadow-sm">Gallery App</p>
-            </div>
+          <MobileView>
+            <div
+              {...gallery}
+              className="img-wrapper flex flex-row         place-items-center justify-center text-white drop-shadow-md select-none"
+            >
+              <div className="absolute text-xl  bg-pink-500 bg-opacity-75 rounded-md p-2">
+                <p className="drop-shadow-sm">Gallery App</p>
+              </div>
 
-            <img src={gallery} alt="photos" />
-            <div className="content text-gray-900">
-            This app uses the Unsplash API. Styling is not too fancy, but app is using masonry grid layout which looks very nice. /Double tap to open.
+              <img src={galleryImg} alt="photos" />
+              <div className="content text-gray-900">
+                This app uses the Unsplash API. Styling is not too fancy, but
+                app is using masonry grid layout which looks very nice.
+              </div>
             </div>
-          </div>
           </MobileView>
           <BrowserView>
-          <a 
-          href="https://gallery-api-unsplash-demo.herokuapp.com/" target="_blank"
-          rel="noopener noreferrer">
-          <div className="img-wrapper flex flex-row         place-items-center justify-center text-white drop-shadow-md select-none">
-            <div className="absolute text-xl  bg-pink-500 bg-opacity-75 rounded-md p-2">
-              <p className="drop-shadow-sm">Gallery App</p>
-            </div>
+            <a
+              href="https://gallery-api-unsplash-demo.herokuapp.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <div className="img-wrapper flex flex-row         place-items-center justify-center text-white drop-shadow-md select-none">
+                <div className="absolute text-xl  bg-pink-500 bg-opacity-75 rounded-md p-2">
+                  <p className="drop-shadow-sm">Gallery App</p>
+                </div>
 
-            <img src={gallery} alt="photos" />
-            <div className="content text-gray-900">
-            This app uses the Unsplash API. Styling is not too fancy, but app is using masonry grid layout which looks very nice.
-            </div>
-          </div>
-          </a>
+                <img src={galleryImg} alt="photos" />
+                <div className="content text-gray-900">
+                  Unsplash API with masonry grid layout
+                </div>
+              </div>
+            </a>
           </BrowserView>
         </Wrapper>
         <Wrapper>
-        <MobileView>
-          <div {...app3}
-           className="img-wrapper flex flex-row         place-items-center justify-center text-white drop-shadow-md select-none">
-            <div 
-            className="absolute text-xl  bg-pink-500 bg-opacity-75 rounded-md p-2">
-              <p className="drop-shadow-sm">Cooking recipes</p>
-            </div>
+          <MobileView>
+            <div
+              {...recipes}
+              className="img-wrapper flex flex-row         place-items-center justify-center text-white drop-shadow-md select-none"
+            >
+              <div className="absolute text-xl  bg-pink-500 bg-opacity-75 rounded-md p-2">
+                <p className="drop-shadow-sm">Cooking recipes</p>
+              </div>
 
-            <img src={recipes} alt="photos" />
-            <div className="content text-gray-900">
-            Create and save cooking recipes. App is using Firestore Database. /Double tap to open.
+              <img src={recipesImg} alt="photos" />
+              <div className="content text-gray-900">
+                Create and save cooking recipes. App is using Firestore
+                Database.
+              </div>
             </div>
-          </div>
           </MobileView>
           <BrowserView>
-          <a 
-          href="https://cooking-recipes-db254.web.app/" target="_blank"
-          rel="noopener noreferrer">
-          <div className="img-wrapper flex flex-row         place-items-center justify-center text-white drop-shadow-md select-none">
-            <div className="absolute text-xl  bg-pink-500 bg-opacity-75 rounded-md p-2">
-              <p className="drop-shadow-sm">Cooking recipes</p>
-            </div>
+            <a
+              href="https://cooking-recipes-db254.web.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <div className="img-wrapper flex flex-row         place-items-center justify-center text-white drop-shadow-md select-none">
+                <div className="absolute text-xl  bg-pink-500 bg-opacity-75 rounded-md p-2">
+                  <p className="drop-shadow-sm">Cooking recipes</p>
+                </div>
 
-            <img src={recipes} alt="photos" />
-            <div className="content text-gray-900">
-            Create and save cooking recipes. App is using Firestore Database.
-            </div>
-          </div>
-          </a>
+                <img src={recipesImg} alt="photos" />
+                <div className="content text-gray-900">
+                  Create and save cooking recipes. App is using Firestore
+                  Database.
+                </div>
+              </div>
+            </a>
           </BrowserView>
         </Wrapper>
       </div>
